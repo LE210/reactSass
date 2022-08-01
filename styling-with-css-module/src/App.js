1096acc1c66498396ef6655f2f68db77f60c3933
LE210 <87165287+LE210@users.noreply.github.com>
2022-08-01 17:36:42 +0900

    reactModuleCSS
    - ...rest
    - moduleCSS
    - moduleSass

---
+++ styling-with-css-module/src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import {useState} from "react";
+import CheckBox from "./components/CheckBox";
+
+function App() {
+  const [check, setCheck] = useState(false);
+
+  const onChange = e => {
+    setCheck(e.target.checked);
+  }
+
+  return (
+      <div>
+        <CheckBox onChange={onChange} checked={check}>
+          다음 약관에 모두 동의하거라
+        </CheckBox>
+        <p>
+          <b>check: </b>
+          {check ? 'true' : 'false'}
+        </p>
+      </div>
+  );
+}
+
+export default App;
